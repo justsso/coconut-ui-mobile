2f97b60d68c7f65cdf26f5b97d82947faef611cd
sso just <528226249@qq.com>
2019-08-26 23:15:13 +0800

    skeleton完结

---
+++ components/skeleton/PropsType.tsx
@@ -0,0 +1,4 @@
+export interface SkeletonProps {
+    loading?: Boolean,  //是否加载完毕
+    action?: Boolean  //是否展示动画效果
+}
--- examples/page2/page2.tsx
+++ examples/page2/page2.tsx
@@ -1,18 +1,52 @@
 import * as React from 'react';
 import Tag from "../../components/tag/tag";
-class PageTwo extends React.Component{
+import Skeleton from "../../components/skeleton/skeleton";
 
-    render(){
+class PageTwo extends React.Component {
+
+    render() {
         return (
             <div>
-                <Tag onClick= { (selected: boolean) => {
+                <Tag onClick={(selected: boolean) => {
                     console.log(selected);
                     console.log('jiating');
-                    return ;
+                    return;
                 }}>家庭</Tag>
                 <Tag
-                onClick={(selected: boolean)=> {console.log(selected)}}
+                    onClick={(selected: boolean) => {
+                        console.log(selected)
+                    }}
                 >123</Tag>
+                <h6>设置头像</h6>
+                <Skeleton
+                    loading={true}
+                    title={false}
+                    avatar avatarShape={"square"}
+                    action={false}
+                />
+                <h6>设置avatar title， 自定义title 宽度</h6>
+                <Skeleton title={true} titleWidth={120} loading  avatar={true} />
+
+                <h6>设置avatar title action</h6>
+                <Skeleton avatar avatarShape={'round'} title={true}  action={true} />
+
+                <h6>设置row   row=3, action</h6>
+                <Skeleton avatar row={3} action title/>
+
+
+                <h6>设置 rowWidth 属性，可以是数组，也可是是单个值rowWidth={'80%'}</h6>
+                <Skeleton avatar row={2} rowWidth={'80%'} />
+
+
+                <h6>设置 rowWidth 属性，可以是数组rowWidth={['100%', '80%']}，也可是是单个值</h6>
+                <Skeleton avatar row={2} rowWidth={['100%', '80%']} />
+
+                <h6>设置 rowProps, rowProps 可以是对象，也可以是对象数组</h6>
+                <Skeleton avatar row={2} rowProps={{width: '100%', height: '14px'}} />
+
+                <Skeleton avatar action row={2}  rowProps={[{width: '40%', height: 10}, {width: '80%', height: 10}]} />
+                <h6>动画: animate</h6>
+                <Skeleton  animate avatar  row={2} title rowProps={[{width: '40%', height: 10}, {width: '80%', height: 10}]} action />
             </div>
         )
     }
