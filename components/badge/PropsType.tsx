6b36e850a7484ebc04b2bbd1835e2389fe0c1925
sso just <528226249@qq.com>
2019-09-01 22:40:22 +0800

    badge

---
+++ components/badge/PropsType.tsx
@@ -0,0 +1,9 @@
+export interface BadgePropsTypes {
+    size ?: 'large' | 'small';
+    overflowCount?: number;
+    corner ?: boolean;
+    dot ?: boolean;
+    text?: any;
+}
+
+//接口是对值所具有的结构进行类型检查
